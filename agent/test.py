56d09e01c249b4fa33bfc15ac06999734a42151f
Ecruz0x <155992994+Ecruz0x@users.noreply.github.com>
2026-05-02 01:54:53 +0100

    Delete agent/test.py

--- agent/test.py
+++
@@ -1,9 +0,0 @@
-import psutil, datetime, ipaddress, ifaddr
-
-def getActiveUsers() -> set[str]:
-		activeUsers = []
-		for user in psutil.users():
-			activeUsers.append(user[0])
-		return activeUsers
-
-print(getActiveUsers())
