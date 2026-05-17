4205cee963aa5ea27bd935288cf0b982598f2742
A.naciri <abdelhaqnaciri0@gmail.com>
2026-05-17 23:58:31 +0100

    Fixed backend schemas issues

--- server/routers/refresh_computers.py
+++ server/routers/refresh_computers.py
@@ -8,12 +8,12 @@ from ..database import dbschema
 from ..database.db import Base, engine, get_db
 from typing import Annotated
 import json
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 
 router = APIRouter()
 
 
-def authenticateComputer(computer_auth: AuthenticateComputer):
+def authenticateComputer(computer_auth: AuthenticateComputer, db: Annotated[Session, Depends(get_db)]):
     result = db.execute(
                 text(f"SELECT 1 FROM computerInfo WHERE computerid = {computer_auth.computer_id} AND fingerprint = '{computer_auth.fingerprint}'")
             )
@@ -27,23 +27,25 @@ def authenticateComputer(computer_auth: AuthenticateComputer):
 
 @router.post("/name", response_model = bool)
 
-def refreshComputerName(newData: RefreshComputerName):
-    is_auth = authenticateComputer(newData.computer_id, newData.fingerprint)
+def refreshComputerName(newData: RefreshComputerName, db: Annotated[Session, Depends(get_db)]):
+    auth_data = {"computerid": newData.computerid, "fingerprint": newData.fingerprint}
+    is_auth = authenticateComputer(auth_data)
     name = newData.newcomputername
     if is_auth:
         result = db.execute(
-                text(f"UPDATE computerInfo SET computername = '{name}' WHERE computerid = {newData.computer_id}")
+                text(f"UPDATE computerInfo SET computername = '{name}' WHERE computerid = {newData.computerid}")
             )
     else:
         raise Exception(AuthenticationError)
 
 @router.post("/mem", response_model = bool)
 
-def refreshMemoryInfo(newData: RefreshMemoryInfo):
-    is_auth = authenticateComputer(newData.computer_id, newData.fingerprint)
+def refreshMemoryInfo(newData: RefreshMemoryInfo, db: Annotated[Session, Depends(get_db)]):
+    auth_data = {"computerid": newData.computerid, "fingerprint": newData.fingerprint}
+    is_auth = authenticateComputer(auth_data)
     if is_auth:
         result = db.execute(
-                text(f"UPDATE memoryinfo SET totalMemory = {newData.totalMemory}, available_memory = {newData.available_memory}, usage = {newData.usage} WHERE computerid = {newData.computer_id}")
+                text(f"UPDATE memoryinfo SET totalMemory = {newData.totalMemory}, available_memory = {newData.available_memory}, usage = {newData.usage} WHERE computerid = {newData.computerid}")
             )
         return True
     else:
