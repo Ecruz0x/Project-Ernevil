b910d1e8da696bec233a7e37f23d548277b5d76a
A.naciri <abdelhaqnaciri0@gmail.com>
2026-05-18 22:38:28 +0100

    Updated Agent software

--- agent/utils/cmpRef.py
+++ agent/utils/cmpRef.py
@@ -13,6 +13,24 @@ server = "http://" + server_data["server_ip"] + ":" + server_data["server_port"]
 
 
 def refreshMemInfo(computerid: int, fingerprint: str, oldData: dict):
+	storedName = oldData["computer_name"]
+	refURL = f"{server}/api/computers/name?computerid={computerid}"
+	currentName = currentComputer.computer_name
+	sent_data = {"computerid": computerid,
+				"fingerprint":fingerprint,
+				"name": currentName}
+
+	if currentName != storedName:
+		oldData["computer_name"] = currentName
+		updateR = requests.post(refURL, data = sent_data)
+		if updateR.status_code <= 201:
+			yield True
+		else:
+			yield sent_data
+
+
+
+def refreshComputerName(computerid: int, fingerprint: str, oldData: dict):
 	storedMemInfo = oldData["memory"]
 	refURL = f"{server}/api/computers/mem?computerid={computerid}"
 	currentMemInfo = currentComputer.getMemoryInfo()
@@ -29,5 +47,19 @@ def refreshMemInfo(computerid: int, fingerprint: str, oldData: dict):
 		else:
 			yield sent_data
 
-
-
+def refreshNetInfo(computerid: int, fingerprint: str, oldData: dict):
+	storedNetInfo = oldData["ip_addr"]
+	refURL = f"{server}/api/computers/mem?computerid={computerid}"
+	currentMemInfo = currentComputer.getMemoryInfo()
+	sent_data = {"computerid": computerid,
+				"fingerprint":fingerprint,
+				"totalMemory": currentMemInfo["totalMemory"],
+				"available_memory": currentMemInfo["availableMemory"],
+				"usage": currentMemInfo["usage"]}
+	if currentMemInfo != storedMemInfo:
+		oldData["memory"] = currentMemInfo
+		updateR = requests.post(refURL, data = sent_data)
+		if updateR.status_code <= 201:
+			yield True
+		else:
+			yield sent_data
